d4cc576544b73207d4065af309b6663365194a02
CHAMPION EMILIEN p1808037 <emilien.champion@etu.univ-lyon1.fr>
2021-04-29 17:14:04 +0200

    added bottom navigation view

--- app/src/main/java/com/example/iotproject/MainActivity.java
+++ app/src/main/java/com/example/iotproject/MainActivity.java
@@ -21,56 +21,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<ClassModel> list = new ArrayList<ClassModel>();
-        ListView listView = findViewById(R.id.listView);
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("cave/sensor/temperature");
-        MyAdapter adapter = new MyAdapter(this, list);
-        listView.setAdapter(adapter);
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                list.clear();
-                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
-                    //list.add(Float.valueOf(snapshot1.getValue().toString()));
-                    System.out.println(snapshot1.getValue().toString());
-                    Float temp = Float.valueOf(snapshot1.getValue().toString());
-                    Float hum = Float.valueOf(snapshot1.getValue().toString());
-                    list.add(new ClassModel(temp,hum));
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        reference = FirebaseDatabase.getInstance().getReference().child("cave/sensor/humidity");
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                int i = 0;
-                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
-                    //list.add(Float.valueOf(snapshot1.getValue().toString()));
-                    //System.out.println(snapshot1.getValue().toString());
-                    Float hum = Float.valueOf(snapshot1.getValue().toString());
-                    if (i < list.size())
-                    {
-                        list.get(i).setHumidite(hum);
-                        ++i;
-                    }
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        System.out.println(reference);
-        System.out.println("Bonjour");
     }
 }
